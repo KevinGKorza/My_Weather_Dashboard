0ea47d13e54bb2ae6ccfd12330953d5ab75f75e8
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-08-10 17:54:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var city = "Hartford";
+var key = 'a5049ba0777ebdbba26cfbab81a6559b';
+var currentDate = moment().format('dddd, MMMM Do YYYY');
+var currentTime = moment().format('YYYY-MM-DD HH:MM:SS');
+var cityHistory = [];
+
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	city = $(this).parent('.btnParameter').siblings('.textValue').val().trim();
+	if (city === "") {
+		return;
+	};
+	cityHist.push(city);
+
+	localStorage.setItem('city', JSON.stringify(cityHistory));
+	fiveForecastEl.empty();
+	getHistory();
+	getWeatherToday();
+});
 
